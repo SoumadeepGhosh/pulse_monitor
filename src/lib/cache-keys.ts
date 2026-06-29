da02decf02b13e35f3a75c54b8bb764ed18d068e
Aditya Dey <adityadey424@gmail.com>
2026-06-29 15:50:01 +0530

    Added cache on success criteria

---
+++ src/lib/cache-keys.ts
@@ -0,0 +1,11 @@
+export const CacheKeys = {
+
+//   monitorDetails: (monitorId: number) =>
+//     `monitor:${monitorId}`,
+
+  successCriteria: (userId: number) =>
+    `criteria:${userId}`,
+
+//   notifications: (userId: number) =>
+//     `notifications:${userId}`,
+};
